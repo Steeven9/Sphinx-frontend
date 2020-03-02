b16b3d5e3e7290cdd440180ffea0a4a2374c4a82
Aron.DallePezze <aron.dalle.pezze@usi.ch>
2020-03-02 16:43:08 +0100

    added all components file and their super basic skeleton

---
+++ src/components/AddDevice.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../App.css';
+
+class AddDevice extends React.Component {
+
+    constructor(props) {
+		super(props);
+		this.state = {
+			// placeholder
+		}
+	}
+
+    render() {
+        return (
+            <div className="addDevice">
+                html page code here
+            </div>
+        );
+    }
+}
+
+
+export default AddDevice;
