7ccd996e3ef92fe6091c0a2caef114e9394bd7ed
Aron.DallePezze <aron.dalle.pezze@usi.ch>
2020-03-02 14:57:29 +0100

    added router in App.js

--- react_sources/src/App.js
+++ react_sources/src/App.js
@@ -1,26 +1,101 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Homepage from './Homepage';
+import Login from './Login';
+import Signup from './Signup';
+import ResetPassword from './ResetPassword';
+import Home from './Home';
+import House from './House';
+import HandleRooms from './HandleRooms';
+import AddRoom from './AddRoom';
+import Room from './Room';
+import AddRoomDevice from './AddRoomDevice';
+import Devices from './Devices';
+import AddDevice from './AddDevice';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            toX: false
+        }
+    }
+
+    toX = (x) => {
+        this.setState({
+            toX: true,
+        });
+    }
+
+    render() {
+        return (
+            <Router>
+                {
+                    this.state.toX ? <Redirect to='/X' /> : <React.Fragment /> 
+                }
+                <div>
+                    <Switch>
+                        <Route path="/">
+                            <Homepage />
+                        </Route>
+
+                        <Route path="/login">
+                            <Login />
+                        </Route>
+
+                        <Route path="/signup">
+                            <Signup />
+                        </Route>
+
+                        <Route path="/reset">
+                            <ResetPassword />
+                        </Route>
+
+                        <Route path="/home">
+                            <Home />
+                        </Route>
+
+                        <Route path="/house">
+                            <House />
+                        </Route>
+
+                        <Route path="/handleRooms">
+                            <HandleRooms />
+                        </Route>
+
+                        <Route path="/addRoom">
+                            <AddRoom />
+                        </Route>
+
+                        <Route path="/room">
+                            <Room />
+                        </Route>
+
+                        <Route path="/addRoomDevice">
+                            <AddRoomDevice />
+                        </Route>
+
+                        <Route path="/devices">
+                            <Devices />
+                        </Route>
+
+                        <Route path="/addDevice">
+                            <AddDevice />
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
