5b0fdd38e01b3a117fa61875aa917c5ff9f71051
Aron.DallePezze <aron.dalle.pezze@usi.ch>
2020-03-05 10:55:21 +0100

    header's issue completed, and also moved the footer which has however to be fixed, but that will be a different issue

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './App.css';
 import 'materialize-css'; // It installs the JS asset only
 import 'materialize-css/dist/css/materialize.min.css';
-
+import LoggedInHeader from './components/LoggedInHeader';
+import NotLoggedInHeader from './components/NotLoggedInHeader';
 import Homepage from './components/Homepage';
 import Login from './components/Login';
 import Signup from './components/Signup';
@@ -15,6 +16,7 @@ import Room from './components/Room';
 import Devices from './components/Devices';
 import AddDevice from './components/AddDevice';
 import Template from './components/Template';
+import Footer from './components/Footer';
 
 import {
     BrowserRouter as Router,
@@ -29,6 +31,7 @@ class App extends React.Component {
         super(props);
 
         this.state = {
+            loggedIn: false,
             toX: false
         }
     }
@@ -45,7 +48,12 @@ class App extends React.Component {
                 {
                     this.state.toX ? <Redirect to='/' /> : <React.Fragment /> 
                 }
+
                 <div>
+                    {
+                        this.state.loggedIn ? <LoggedInHeader /> : <NotLoggedInHeader /> 
+                    }
+
                     <Switch>
                         <Route path="/login">
                             <Login />
@@ -94,9 +102,9 @@ class App extends React.Component {
                         <Route path="/">
                             <Homepage />
                         </Route>
-
-
                     </Switch>
+
+                    <Footer />
                 </div>
             </Router>
         );
--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../App.css';
-import logo from "./img/logo-horizontal.svg";
 
 class Template extends React.Component {
 
@@ -14,21 +13,6 @@ class Template extends React.Component {
     render() {
         return (
             <div id="wrapper" className="template">
-                <header>
-                    <nav className="navbar-fixed">
-                        <div className="nav-wrapper">
-                            <a href="/" className="brand-logo">
-                                <img className="nav-left nav-logo-horizontal responsive-img" src={logo} alt="SmartHut logo" />
-                            </a>
-                            <ul className="right nav-menu-desktop-right hide-on-med-and-down">
-                                <li><a href="rooms">My rooms</a></li>
-                                <li><a href="devices">My devices</a></li>
-                                <li><a href="#">Log out</a></li>
-                            </ul>
-                        </div>
-                    </nav>
-                </header>
-
                 <main>
                     <article>
                         <div id="content" className="container">
@@ -55,12 +39,6 @@ class Template extends React.Component {
                         </div>
                     </article>
                 </main>
-
-                <footer className="page-footer">
-                    <div className="footer-copyright">
-                        <span>&copy;2020 Sphinx Software</span>
-                    </div>
-                </footer>
             </div>
         );
     }
