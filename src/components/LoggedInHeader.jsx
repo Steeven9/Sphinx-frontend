616935b4ef062aa33dfe1c0a9768883b96190f83
garroe <garroe@usi.ch>
2020-03-05 13:32:45 +0100

    Added the 'navbar-fixed' class to both headers to make them sticky.

--- src/components/LoggedInHeader.jsx
+++ src/components/LoggedInHeader.jsx
@@ -14,18 +14,20 @@ class LoggedInHeader extends React.Component {
     render() {
         return (
             <header id="page-header" className="loggedInHeader">
-                <nav className="navbar-fixed">
-                    <div className="nav-wrapper">
-                        <a href="/home" className="brand-logo">
-                            <img className="nav-left nav-logo-horizontal responsive-img" src={logo} alt="SmartHut logo" />
-                        </a>
-                        <ul className="right nav-menu-desktop-right hide-on-med-and-down">
-                            <li><a href="rooms">My rooms</a></li>
-                            <li><a href="devices">My devices</a></li>
-                            <li><a href="#">Log out</a></li>
-                        </ul>
-                    </div>
-                </nav>
+                <div className="navbar-fixed">
+                    <nav className="navbar-fixed">
+                        <div className="nav-wrapper">
+                            <a href="/home" className="brand-logo">
+                                <img className="nav-left nav-logo-horizontal responsive-img" src={logo} alt="SmartHut logo" />
+                            </a>
+                            <ul className="right nav-menu-desktop-right hide-on-med-and-down">
+                                <li><a href="rooms">My rooms</a></li>
+                                <li><a href="devices">My devices</a></li>
+                                <li><a href="#">Log out</a></li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
             </header>
         );
     }
--- src/components/LoggedOutHeader.jsx
+++ src/components/LoggedOutHeader.jsx
@@ -14,17 +14,19 @@ class LoggedOutHeader extends React.Component {
     render() {
         return (
             <header id="page-header" className="loggedOutHeader">
-                <nav className="navbar-fixed">
-                    <div className="nav-wrapper">
-                        <a href="/" className="brand-logo">
-                            <img className="nav-left nav-logo-horizontal responsive-img" src={logo} alt="SmartHut logo" />
-                        </a>
-                        <ul className="right nav-menu-desktop-right hide-on-med-and-down">
-                            <li><a href="signup">Create account</a></li>
-                            <li><a href="login">Log in</a></li>
-                        </ul>
-                    </div>
-                </nav>
+                <div className="navbar-fixed">
+                    <nav className="navbar-fixed">
+                        <div className="nav-wrapper">
+                            <a href="/" className="brand-logo">
+                                <img className="nav-left nav-logo-horizontal responsive-img" src={logo} alt="SmartHut logo" />
+                            </a>
+                            <ul className="right nav-menu-desktop-right hide-on-med-and-down">
+                                <li><a href="signup">Create account</a></li>
+                                <li><a href="login">Log in</a></li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
             </header>
         );
     }
