2ad8b52d783e1f71f68440cb378610eca7c7b3b6
luinia <luinia@usi.ch>
2020-03-08 13:32:00 +0100

    login signup reset html views

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -15,8 +15,39 @@ class Signup extends React.Component {
      */
     render() {
         return (
-            <div className="signup">
-                html page code here
+            <div className="login" >
+                <section className="content-box sample-box z-depth-2">
+                <div>
+                    <div>
+                        <h2 className="center"> Create account </h2>
+                    </div>
+                    <br/>
+                    <div className="flex-raw">
+                        <input type="text" placeholder="First name*" />
+                        <input type="text" placeholder="Last name*" />
+                    </div>
+                    <div className="flex-raw">
+                        <input type="email" placeholder="Email*"/>
+                        <input type="text" placeholder="Username*"/>    
+                    </div>
+                    <div className="flex-raw">
+                        <input type="password" placeholder="Password*"/>
+                        <input type="password" placeholder="Confirm password*"/>
+                    </div>
+                </div>
+                
+                <br/> <br/>
+                <div>
+                    <div className="flex-raw">
+                        <a href="/login"><b>Login</b></a>
+                        <form action="/create" method="POST">
+                            <button className="login-btn">
+                                <b>Create</b>
+                            </button>
+                        </form>
+                    </div>
+                </div>
+                </section>
             </div>
         );
     }
