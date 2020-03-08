0a203683bb87219521218b390a97bc51fac0c373
luinia <luinia@usi.ch>
2020-03-08 13:32:00 +0100

    login signup reset html views

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -15,8 +15,35 @@ class Login extends React.Component {
      */
     render() {
         return (
-            <div className="login">
-                html page code here
+            <div className="login" >
+                <section className="content-box sample-box z-depth-2">
+                <div>
+                    <div>
+                        <h2 className="center">Log in</h2>
+                    </div>
+                    <br/>
+                    <div className='flex-column center'>
+                        <input type="text" placeholder="Username" />
+                        <br/>
+                        <input type="password" placeholder="Password" />
+                    </div>
+                </div>
+                <br/>
+                <div className="center">
+                    <a href="/reset"><i>Reset password</i></a>
+                </div>
+                <br/> <br/>
+                <div>
+                    <div className="flex-raw">
+                        <a href="/signup"><b>Create account</b></a>
+                        <form action="/login" method="POST">
+                            <button  className="login-btn">
+                               <b>Log in</b> 
+                            </button>
+                        </form>
+                    </div>
+                </div>
+                </section>
             </div>
         );
     }
--- src/components/ResetPassword.jsx
+++ src/components/ResetPassword.jsx
@@ -15,8 +15,34 @@ class ResetPassword extends React.Component {
      */
     render() {
         return (
-            <div className="resetPassword">
-                html page code here
+            <div className="login" >
+                <section className="content-box sample-box z-depth-2">
+                <div>
+                    <div>
+                        <h2 className="center">Restore password</h2>
+                    </div>
+                    <br/>
+                    <div className=" instructions center">
+                        Type the email address registered to your account. If we find it in our records, you’ll receive the instructionsto restore your password.
+                    </div>
+                    <br/> 
+                    <div className='flex-column center'>
+                        <input type="text" placeholder="E-mail" />
+                    </div>
+                </div>
+                <br/>
+                <br/> <br/>
+                <div>
+                    <div className="flex-raw">
+                        <a href="/login"><b>cancel</b></a>
+                        <form action="/login" method="POST">
+                            <button className="login-btn">
+                               <b>Reset</b> 
+                            </button>
+                        </form>
+                    </div>
+                </div>
+                </section>
             </div>
         );
     }
