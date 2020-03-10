9ea28de15fd0ec207203493e1ed62fa314cb940d
andrea di micco <dimica@usi.ch>
2020-03-10 15:31:29 +0100

    addRoom

--- src/components/AddRoom.jsx
+++ src/components/AddRoom.jsx
@@ -14,10 +14,24 @@ class AddRoom extends React.Component {
      * TODO
      */
     render() {
-        return (
-            <div className="addRoom">
-                html page code here
-            </div>
+        return (    
+            // <body>            
+                <div className="addRoom">
+                    <div class="content-box">
+                        <h2 class="title">Add room</h2>
+                        <div class="dates"> 
+                            <div class="dates-input"><input style={{width: 300 + 'px'}} type="text" name="" placeholder="Room Name*"/></div>
+                           <div class="dates-input"><input style={{width: 300 + 'px'}} type="text" name="" placeholder="Room Type*" /></div>
+                           <div class="dates-input"><input style={{width: 300 + 'px'}} type="number" name="" placeholder="Floor*" /></div>
+                        </div>
+                        <div class="dates">
+                           <div class="dates-input"><button type="button" name="button" class="btn-secondary btn">Cancel</button></div>
+                            <div class="dates-input"><button type="button" name="button" class="btn-primary btn">Save room</button></div>
+                        </div>
+                    </div>
+                </div>
+            // </body>
+
         );
     }
 }
