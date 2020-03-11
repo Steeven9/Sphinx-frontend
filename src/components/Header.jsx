e6cabb0a8b1ef7157a9dff72ecea925977066cfb
Aron.DallePezze <aron.dalle.pezze@usi.ch>
2020-03-11 16:24:51 +0100

    better implementation of the fix in order to avoid code duplication

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -33,32 +33,29 @@ class Header extends React.Component {
             <header id="page-header" className="header">
                 <div className="navbar-fixed">
                         <nav className="navbar-fixed">
-                            {
-                                this.state.loggedIn ? 
+                            <div className="nav-wrapper">
+                                <a href={this.state.loggedIn ? "/home" : "/"} className="brand-logo">
+                                    <img className="nav-left nav-logo-horizontal responsive-img" src={logo} alt="SmartHut logo" />
+                                </a>
 
-                                <div className="nav-wrapper">
-                                    <a href="/home" className="brand-logo">
-                                        <img className="nav-left nav-logo-horizontal responsive-img" src={logo} alt="SmartHut logo" />
-                                    </a>
+                                {
+                                    this.state.loggedIn ? 
+
                                     <ul className="right nav-menu-desktop-right hide-on-med-and-down">
                                         <li><a href="/rooms">My rooms</a></li>
                                         <li><a href="/devices">My devices</a></li>
                                         <li><a href="/template">Log out</a></li>
                                     </ul>
-                                </div>
                     
-                                :
+                                    :
 
-                                <div className="nav-wrapper">
-                                    <a href="/" className="brand-logo">
-                                        <img className="nav-left nav-logo-horizontal responsive-img" src={logo} alt="SmartHut logo" />
-                                    </a>
                                     <ul className="right nav-menu-desktop-right hide-on-med-and-down">
                                         <li><a href="/signup">Create account</a></li>
                                         <li><a href="/login">Log in</a></li>
                                     </ul>
-                                </div>
-                            }
+                                }
+
+                            </div>
                         </nav>
                     </div>
             </header>
