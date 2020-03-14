7fd7bc3abb33a91bd6f85de60a348fa47ff2686e
andrea di micco <dimica@usi.ch>
2020-03-14 16:34:48 +0100

    addRoom-addDevice

--- src/components/AddDevice.jsx
+++ src/components/AddDevice.jsx
@@ -16,7 +16,19 @@ class AddDevice extends React.Component {
     render() {
         return (
             <div className="addDevice">
-                html page code here
+                <div class="content-box">
+                        <h2 class="title">Add device</h2>
+                        <div class="dates"> 
+                            <div class="dates-input"><input style={{width: 300 + 'px'}} type="text" name="" placeholder="Device type*"/></div>
+                           <div class="dates-input"><input style={{width: 300 + 'px'}} type="text" name="" placeholder="Costum name*" /></div>
+                           <div class="dates-input"><input style={{width: 300 + 'px'}} type="text" name="" placeholder="Room*" /></div>
+                           <div class="dates-input"><input style={{width: 300 + 'px'}} type="text" name="" placeholder="couple with device" /></div>
+                        </div>
+                        <div class="dates">
+                           <div class="dates-input"><button type="button" name="button" class="btn-secondary btn">Cancel</button></div>
+                            <div class="dates-input"><button type="button" name="button" class="btn-primary btn">Save room</button></div>
+                        </div>
+                    </div>
             </div>
         );
     }
