f3a5c93a57b1b59bb26832ad3b1d3ace6e840599
andrea di micco <dimica@usi.ch>
2020-03-18 16:18:54 +0100

    house page

--- src/components/House.jsx
+++ src/components/House.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../App.css';
+import imgGarage from './img/icons/rooms/icon-garage.svg';
 
 class House extends React.Component {
 
@@ -16,7 +17,30 @@ class House extends React.Component {
     render() {
         return (
             <div className="house">
-                html page code here
+                <div class="content-box">
+                    <div class="canvas1">
+                       <h2>My rooms</h2>
+                       <button class="btn-primary-circular add-btn">+ Add room</button>
+                    </div>
+
+                    <div class="canvas2">
+                        <div class="legenda"><div class="name1">Name</div> <div class="floor1">Floor</div> <div class="device1">Devices</div></div>
+                        <hr class="line"/>
+                        <div class="room">
+                            <div class="image vertical-center"><img src={imgGarage} alt="device-logo"/></div>
+                            <div class="room-name vertical-center"> name of the room</div>
+                            <div class="flo-dev-number vertical-center">2</div>
+                            <div class="flo-dev-number vertical-center">7</div>
+                            <div class="room-button1 vertical-center"> <button><img src="" alt="im"/></button></div>
+                            <div class="room-button2 vertical-center"> <button><img src="" alt="im"/></button></div>
+
+                        </div>
+
+                        
+
+                        <hr class="line"/>
+                    </div>
+                </div>
             </div>
         );
     }
