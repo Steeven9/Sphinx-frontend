3e26d60d5c1ce6ecb86f431cb5c98bde4dc102ad
luinia <luinia@usi.ch>
2020-03-18 17:27:22 +0100

    file spostato css

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../App.css';
+import '../LoginCreateReset.css'
 
 class Login extends React.Component {
 
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../App.css';
+import '../LoginCreateReset.css'
 
 class Signup extends React.Component {
 
@@ -15,89 +16,42 @@ class Signup extends React.Component {
      */
     render() {
         return (
-            <div
-className="signup">
+            
+            <div className="signup">
 
 
-<div
-class="content-box1">
+                <div class="content-box1">
 
-<h2
-class="title">Create account</h2>
+                    <h2 class="title">Create account</h2>
 
-<div
-class="dates">
+                    <div class="dates">
 
-<div
-class="dates-input"><input
-class="cred"
-type="text"
-name=""
-placeholder="First name"/></div>
+                        <div class="dates-input"><input class="cred" type="text"  name="" placeholder="First name"/></div>
 
-<div
-class="dates-input"><input
-class="cred"
-type="text"
-name=""
-placeholder="Laste name*"
-/></div>
+                        <div class="dates-input"><input class="cred" type="text" name="" placeholder="Laste name*" /></div>
 
-<div
-class="dates-input"><input
-class="cred"
-type="email"
-name=""
-placeholder="Email"
-/></div>
+                        <div class="dates-input"><input class="cred" type="email" name="" placeholder="Email" /></div>
 
-<div
-class="dates-input"><input
-class="cred"
-type="text"
-name=""
-placeholder="Username"
-/></div>
+                        <div class="dates-input"><input class="cred" type="text" name="" placeholder="Username" /></div>
 
-<div
-class="dates-input"><input
-class="cred"
-type="password"
-name=""
-placeholder="Password*"
-/></div>
+                        <div class="dates-input"><input class="cred" type="password" name="" placeholder="Password*" /></div>
 
-<div
-class="dates-input"><input
-class="cred"
-type="password"
-name=""
-placeholder="Confirm*"
-/></div>
+                        <div class="dates-input"><input class="cred" type="password" name="" placeholder="Confirm*" /></div>
 
-</div>
+                    </div>
 
-<div
-class="dates">
+                    <div class="dates">
 
-<div
-class="dates-input"><a href="/login"><button
-type="button"
-name="button"
-class="btn-secondary btn">Sing in</button></a></div>
+                        <div class="dates-input"><a href="/login"><button type="button" name="button"class="btn-secondary btn">Sing in</button></a></div>
 
-<div
-class="dates-input"><button
-type="button"
-name="button"
-class="btn-primary btn">Create account</button></div>
+                        <div class="dates-input"><button type="button" name="button" class="btn-primary btn">Create account</button></div>
 
-</div>
+                    </div>
 
-</div>
+                </div>
 
 
-</div>
+            </div>
         );
     }
 }
