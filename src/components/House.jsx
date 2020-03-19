42a72878f1b68335e328b05f272b78724835a870
andrea di micco <dimica@usi.ch>
2020-03-19 13:12:20 +0100

    fixes

--- src/components/House.jsx
+++ src/components/House.jsx
@@ -21,11 +21,11 @@ class House extends React.Component {
                 <div class="content-box">
                     <div class="canvas1">
                        <h2>My rooms</h2>
-                       <button class="btn-primary-circular2 add-btn">+ Add room</button>
+                       <button class="btn-primary-circular2 add-btn">Add room</button>
                     </div>
 
                     <div class="canvas2">
-                        <div class="legenda"><div class="name1">Name</div> <div class="floor1">Floor</div> <div class="device1">Devices</div></div>
+                        <div class="informations"><div class="name1">Name</div> <div class="floor1">Floor</div> <div class="device1">Devices</div></div>
                         <hr class="line"/>
                         <div class="room">
                             <div class="image vertical-center"><img src={imgGarage} alt="device-logo"/></div>
