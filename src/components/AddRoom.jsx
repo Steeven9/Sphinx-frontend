955cde88019040aa5a23e494efddfc70e80aca9e
garroe <garroe@usi.ch>
2020-04-03 15:42:31 +0200

    46: Added transparency to content-box in 'Add room' page

--- src/components/AddRoom.jsx
+++ src/components/AddRoom.jsx
@@ -87,7 +87,7 @@ class AddRoom extends React.Component {
     render() {
         return (         
             <div className="addRoom">
-                <div className="content-box">
+                <div className="content-box content-box-transparency">
                     <h2 className="title">Add room</h2>
                     <div className="dates">
                         <span className="textFields"><input type="text" name="roomName" placeholder="Room Name"
