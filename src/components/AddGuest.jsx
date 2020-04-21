498616f5597ac8951ad11c8e2cdee72ebf76779a
Aron.DallePezze <aron.dalle.pezze@usi.ch>
2020-04-21 19:03:03 +0200

    #50: complete sendDatas and showError in AddGuest

--- src/components/AddGuest.jsx
+++ src/components/AddGuest.jsx
@@ -6,7 +6,6 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 import withStyles from "@material-ui/core/styles/withStyles";
 
 const ColorCircularProgress = withStyles({root: {color: '#580B71'},})(CircularProgress);
-// var devicesToSend = [];
 
 class AddGuest extends React.Component {
 
@@ -16,6 +15,10 @@ class AddGuest extends React.Component {
             guestUsername: "",
             rooms: null,
             devices: [],
+            scenes: [],
+            error: -1,  // -1 nothing, 0 no guest username, 1 no devices or scenes selected, 2 bad request, 3 unexpected error
+            errorType: "",
+            isLoading: false,
         }
     }
 
@@ -88,7 +91,7 @@ class AddGuest extends React.Component {
                     :
                     <p><b>There are no devices in this room.</b></p>
                 }
-                <br/><br/>
+                <br/>
                 </React.Fragment>
             );
         }
@@ -122,7 +125,63 @@ class AddGuest extends React.Component {
 
     sendDatas = evt => {
         evt.preventDefault();
-        console.log("Datas Sent")
+
+        if (this.state.guestUsername === "") {
+            this.setState({error: 0})
+            return
+        }
+        else if (this.state.devices.length === 0 && this.state.scenes.length === 0) {
+            this.setState({error: 1})
+            return
+        }
+        else {
+            this.setState({isLoading: true, error: -1})
+            fetch('http://localhost:8080/guests/', {
+                method: 'POST',
+                headers: { 
+                    'user': this.state.username,
+                    'session-token': this.state.session_token,
+                    'Accept': 'application/json', 
+                    'Content-Type': 'application/json' 
+                },
+                body: JSON.stringify({
+                    guest: this.state.guestUsername,
+                    devices: this.state.devices,
+                    scenes: this.state.scenes
+                })
+            })
+            .then( (res) => {
+                this.setState({isLoading: false})
+                if (res.status === 200) {
+                    this.redirectToGuests()
+                }
+                else if (res.status === 401) {
+                    this.props.logOut(1)
+                }
+                else if (res.status === 400) {
+                    this.setState({error: 2})
+                }
+                else {
+                    this.setState({error: 3, errorType: "Error Code: " + res.status})
+                }
+            })
+            .catch( e => this.setState({isLoading: false, error: 3, errorType: e.toString()}))
+        }
+    }
+
+    showError = () => {
+        if (this.state.error === 0) {
+            return (<span className="error-message">Please fill the guest's username</span>)
+        }
+        else if (this.state.error === 1) {
+            return (<span className="error-message">Please select at least one device or scene</span>)
+        }
+        else if (this.state.error === 2) {
+            return (<span className="error-message">Error: bad request</span>)
+        }
+        else if (this.state.error === 3) {
+            return (<span className="error-message">{this.state.errorType}</span>)
+        }
     }
 
     /**
@@ -140,6 +199,13 @@ class AddGuest extends React.Component {
                     </div>
                     <br/>
                     {this.mapRooms()}
+
+                    <div className="message-two-lines center-text">
+                        <span>
+                            <ColorCircularProgress className={this.state.isLoading ? "loading-spinner" : "hidden"}/>
+                        </span>
+                        {this.showError()}
+                    </div>
                     <button type="button" name="button" className="btn-secondary btn waves-effect waves-light" onClick={this.redirectToGuests}>Cancel</button>
                     <button type="button" name="button" className="Handle-btn-primary btn waves-effect waves-light" onClick={this.sendDatas}>Save</button>
                 </div>
