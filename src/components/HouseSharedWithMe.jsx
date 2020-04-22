865f52b44125b8af1065e5905d776e6747c614a8
Aron.DallePezze <aron.dalle.pezze@usi.ch>
2020-04-22 14:49:02 +0200

    #50: complete SharedWithMe.jsx - WIP HouseSharedWithMe.jsx

--- src/components/HouseSharedWithMe.jsx
+++ src/components/HouseSharedWithMe.jsx
@@ -13,27 +13,26 @@ class HouseSharedWithMe extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            guestUsername: "",
+            ownerUsername: "",
             rooms: null,
             devices: [],
             scenes: [],
             error: -1,  // -1 nothing, 0 no devices or scenes selected, 1 bad request, 2 unexpected error
             errorType: "",
             isLoading: false,
-            guestDevices: [],
         }
     }
 
     componentDidMount() {
         const parsed = qs.parse(window.location.search);
-        let parsedGuest = parsed.guest
-        this.setState({guestUsername: parsedGuest})
+        let parsedOwner = parsed.owner
+        this.setState({ownerUsername: parsedOwner})
 
         document.addEventListener("keydown", (evt) => {
             if (evt.key === 'Enter') this.sendDatas(evt)
         });
 
-        fetch('http://localhost:8080/guests/' + this.props.username + '/devices/' + parsedGuest, {
+        fetch('http://localhost:8080/guests/' + parsedOwner + '/devices/' + this.props.username, {
             method: 'GET',
             headers: {
                 'user': this.props.username,
@@ -49,36 +48,8 @@ class HouseSharedWithMe extends React.Component {
         })
         .then((data) => {
             if (data === null) return;
-            let devices = JSON.parse(data);
-            this.setState({guestDevices: devices})
-        });
-
-        fetch('http://localhost:8080/rooms/', {
-            method: 'GET',
-            headers: {
-                'user': this.props.username,
-                'session-token': this.props.session_token,
-            },
-        })
-        .then((res) => {
-            if (res.status === 401) {
-                this.props.logOut(1);
-            } else if (res.status === 200) {
-                return res.text();
-            } else {
-                return null;
-            }
-        })
-        .then((data) => {
-            let response = JSON.parse(data);
-
-            if (response === null) {
-                //TODO error handling
-            } else {
-                this.setState({
-                    rooms: response,
-                });
-            }
+            let ownerDevices = JSON.parse(data);
+            this.setState({devices: ownerDevices})
         });
     }
 
@@ -86,7 +57,7 @@ class HouseSharedWithMe extends React.Component {
      * Maps the received array of rooms and sets it as this.state.rooms. If no rooms are available, this.state.rooms gets changed with a specific phrase.
      * @param rooms: array of rooms
      */
-    mapRooms = () => {
+    mapRooms = () => {  //TODO
         const { rooms } = this.state;
         if (!rooms) { 
             return <div className="message-two-lines center-text"><span><ColorCircularProgress className="loading-spinner"/></span></div>
@@ -126,112 +97,6 @@ class HouseSharedWithMe extends React.Component {
         }
     }
 
-    handleCheckboxDevice = (deviceID) => {
-        let devices = this.state.devices
-        if (devices.indexOf(deviceID) !== -1) {
-            let deviceIndex = devices.indexOf(deviceID);
-            let toSet = devices.splice(0, deviceIndex).concat(devices.splice(deviceIndex+1, devices.length + 1))
-            this.setState({devices: toSet})
-        }
-        else {
-            devices = [...this.state.devices, deviceID]
-            this.setState({devices: devices})
-        }
-    }
-
-    // handleCheckboxRoom = (roomID) => {
-    //     console.log(document.getElementById(roomID))
-    // }
-
-    //Redirection to /guests
-    redirectToGuests = () => {
-        window.location.href = '/guests'
-    }
-
-    sendDatas = evt => {
-        evt.preventDefault();
-
-        if (this.state.devices.length === 0 && this.state.scenes.length === 0) {
-            this.setState({error: 0})
-            return
-        }
-        else {
-            this.setState({isLoading: true, error: -1})
-            fetch('http://localhost:8080/guests/', {
-                method: 'PUT',
-                headers: { 
-                    'user': this.state.username,
-                    'session-token': this.state.session_token,
-                    'Accept': 'application/json', 
-                    'Content-Type': 'application/json' 
-                },
-                body: JSON.stringify({
-                    guest: this.state.guestUsername,
-                    devices: this.state.devices,
-                    scenes: this.state.scenes
-                })
-            })
-            .then( (res) => {
-                this.setState({isLoading: false})
-                if (res.status === 200) {
-                    this.redirectToGuests()
-                }
-                else if (res.status === 401) {
-                    this.props.logOut(1)
-                }
-                else if (res.status === 400) {
-                    this.setState({error: 1})
-                }
-                else {
-                    this.setState({error: 2, errorType: "Error Code: " + res.status})
-                }
-            })
-            .catch( e => this.setState({isLoading: false, error: 2, errorType: e.toString()}))
-        }
-    }
-
-    deleteGuest = evt => {
-        this.setState({isLoading: true, error: -1})
-        fetch('http://localhost:8080/guests/' + this.state.guestUsername, {
-            method: 'DELETE',
-            headers: { 
-                'user': this.state.username,
-                'session-token': this.state.session_token,
-            }
-        })
-        .then( (res) => {
-            this.setState({isLoading: false})
-            if (res.status === 204) {
-                this.redirectToGuests()
-            }
-            else if (res.status === 401) {
-                this.props.logOut(1)
-            }
-            else if (res.status === 400) {
-                this.setState({error: 1})
-            }
-            else {
-                this.setState({error: 2, errorType: "Error Code: " + res.status})
-            }
-        })
-        .catch( e => {
-            this.setState({isLoading: false})
-            this.setState({error: 2, errorType: e.toString()})
-        })
-    }
-
-    showError = () => {
-        if (this.state.error === 0) {
-            return (<span className="error-message">Please select at least one device or scene</span>)
-        }
-        else if (this.state.error === 1) {
-            return (<span className="error-message">Error: bad request</span>)
-        }
-        else if (this.state.error === 2) {
-            return (<span className="error-message">{this.state.errorType}</span>)
-        }
-    }
-
     /**
      * Renders the list of rooms and device plus the form for the guest's username
      */
@@ -240,19 +105,9 @@ class HouseSharedWithMe extends React.Component {
             <div className="container">
                 <div className="rooms-content-box z-depth-2">
                     <div className="headline-box row row-collapsible row row-collapsible-custom">
-                        <h2 className="col l11 left-align headline-title">Edit guest</h2>
+                        <h2 className="col l11 left-align headline-title">{this.state.ownerUsername}'s House</h2>
                     </div>
                     {this.mapRooms()}
-
-                    <div className="message-two-lines center-text">
-                        <span>
-                            <ColorCircularProgress className={this.state.isLoading ? "loading-spinner" : "hidden"}/>
-                        </span>
-                        {this.showError()}
-                    </div>
-                    <button type="button" name="button" className="btn-secondary btn waves-effect waves-light" onClick={this.redirectToGuests}>Cancel</button>
-                        <button type="button" name="button" className="Handle-btn-secondary btn waves-effect waves-light" onClick={this.deleteGuest}>Delete</button>
-                    <button type="button" name="button" className="Handle-btn-primary btn waves-effect waves-light" onClick={this.sendDatas}>Save</button>
                 </div>
             </div>
         );
--- src/components/SharedWithMe.jsx
+++ src/components/SharedWithMe.jsx
@@ -11,12 +11,12 @@ class SharedWithMe extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            guests: <div className="message-two-lines center-text"><span><ColorCircularProgress className="loading-spinner"/></span></div>
+            owners: <div className="message-two-lines center-text"><span><ColorCircularProgress className="loading-spinner"/></span></div>
         }
     }
 
     componentDidMount() {
-        fetch('http://localhost:8080/guests/', {
+        fetch('http://localhost:8080/guests/houses', {
             method: 'GET',
             headers: {
                 'user': this.props.username,
@@ -36,65 +36,64 @@ class SharedWithMe extends React.Component {
             let response = JSON.parse(data);
 
             if (response === null) {
-                this.setState({guests: <p><b>An error has occurred.</b></p>});
+                this.setState({owners: <p><b>An error has occurred.</b></p>});
             } else if (response.length === 0) {
                 this.setState({
-                    guests: <p><b>You haven't authorized any guest yet. Click on the + button to authorize one.</b></p>
+                    owners: <p><b>You're no-one guest.</b></p>
                 });
             } else {
-                this.mapGuests(response);
+                this.mapOwners(response);
             }
         })
-        .catch(e => this.setState({guests: <p><b>Error.</b></p>}))
+        .catch(e => this.setState({owners: <p><b>Error.</b></p>}))
     }
 
     /**
-     * Maps the received array of guests and sets it as this.state.guests. If no guests are available, this.state.guests gets changed with a specific phrase.
-     * @param guests: array of guests
+     * Maps the received array of owners and sets it as this.state.owners. If no owners are available, this.state.owners gets changed with a specific phrase.
+     * @param owners: array of owners
      */
-    mapGuests = (guests) => {
-        if (guests.length === 0) {
-            this.setState({guests: <p><b>You haven't authorized any guest yet. Click on the + button to authorize one.</b></p>});
+    mapOwners = (owners) => {
+        if (owners.length === 0) {
+            this.setState({owners: <p><b>You're no-one guest.</b></p>});
         } else {
             let i = 0;
-            let toSet = guests.map((guest) =>
+            let toSet = owners.map((owner) =>
                 <div key={i++} className="row room">
                     <div className="col l1 image vertical-center"><i className="material-icons account-circle">account_circle</i></div>
-                    <div className="col l5 vertical-center">{guest.username}</div>
+                    <div className="col l5 vertical-center">{owner.username}</div>
                     <div className="col l2"></div>
-                    <div className="col l1 room-button1 vertical-center">
+                    <div className="col l1 room-button2 vertical-center">
                         <i className="material-icons btn-edit"
-                           onClick={() => this.redirectToEditGuest(guest.username)}>edit</i>
+                           onClick={() => this.redirectToHouseSharedWithMe(owner.username)}>visibility_outlined</i>
                     </div>
                 </div>
             );
-            this.setState({guests: toSet})
+            this.setState({owners: toSet})
         }
     }
 
     //Redirection to /editGuest
-    redirectToEditGuest = (guestUsername) => {
-        window.location.href = '/editGuest?username=' + guestUsername
+    redirectToHouseSharedWithMe = (ownerUsername) => {
+        window.location.href = '/houseSharedWithMe?owner=' + ownerUsername
     }
 
     /**
-     * Renders the list of guests
+     * Renders the list of owners
      */
     render() {
         return (
             <div className="container">
                 <div className="rooms-content-box z-depth-2">
                     <div className="headline-box row row-collapsible row row-collapsible-custom">
-                        <h2 className="col l11 left-align headline-title">My guests</h2>
-                        <a href="/addGuest"><i className="col col-collapsible l1 btn waves-effect waves-light btn-primary-circular right material-icons">add</i></a>
+                        <h2 className="col l11 left-align headline-title">Houses shared with me</h2>
                     </div>
 
                     <div className="row rooms-headline">
                         <div className="col l1"></div>
-                        <div className="col l5">Username</div>
+                        <div className="col l5">Owner</div>
                         <div className="col l4"></div>
                     </div>
-                    {this.state.guests}
+                    {this.state.owners}
                 </div>
             </div>
         );
