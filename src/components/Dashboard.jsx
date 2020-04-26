0b4b8e8cdde43875017294df56fb31feb9954ac4
garroe <garroe@usi.ch>
2020-04-26 21:20:46 +0200

    #66: Fixed Dashboard alignment and cards corners

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -6,6 +6,7 @@ import Paper from '@material-ui/core/Paper';
 const useStyles = makeStyles((theme) => ({
     paper: {
         padding: theme.spacing(2),
+        borderRadius: 16,
         spacing: 5,
         textAlign: 'center',
         height: 250,
@@ -20,58 +21,78 @@ function Dashboard() {
     const classes = useStyles();
 
     return (
-        <Grid container className='dashboard-nav' xs={9} spacing={3} flexGrow={1} justify={'center'}
-              alignItems={'center'}>
-            <Grid item>
-                <a href="/devices">
-                    <Paper className={classes.paper}>
-                        <img alt="Devices" className="dashboard-nav-img" src="/img/icons/dashboard/icon-all-devices-bw.svg"/>
-                        <p>Devices</p>
-                    </Paper>
-                </a>
+        <div className='container'>
+            <Grid container
+                  className='dashboard-nav'
+                  padding={4}
+                  spacing={6}
+                  wrap={'wrap'}
+                  alignItems={'center'}
+                  align={'center'}
+                  justify={'center'}
+                  style={{
+                      margin: 0,
+                      width: '100%',
+                  }}>
+                <Grid item lg={4} md={6} xs={12}>
+                    <a href="/devices">
+                        <Paper className={classes.paper}>
+                            <img alt="Devices" className="dashboard-nav-img"
+                                 src="/img/icons/dashboard/icon-all-devices-bw.svg"/>
+                            <p>Devices</p>
+                        </Paper>
+                    </a>
+                </Grid>
+                <Grid item lg={4} md={6} xs={12}>
+                    <a href="/house">
+                        <Paper className={classes.paper}>
+                            <img alt="Rooms" className="dashboard-nav-img"
+                                 src="/img/icons/dashboard/icon-all-rooms-bw.svg"/>
+                            <p>Rooms</p>
+                        </Paper>
+                    </a>
+                </Grid>
+                <Grid item lg={4} md={6} xs={12}>
+                    <a href="/scenes">
+                        <Paper className={classes.paper}>
+                            <img alt="Scenes" className="dashboard-nav-img"
+                                 src="/img/icons/dashboard/icon-scenes-bw.svg"/>
+                            <p>Scenes</p>
+                        </Paper>
+                    </a>
+                </Grid>
+                <Grid item lg={4} md={6} xs={12}>
+                    <a href="/automations">
+                        <Paper className={classes.paper}>
+                            <img alt="automations" className="dashboard-nav-img"
+                                 src="/img/icons/dashboard/icon-automation-bw.svg"/>
+                            <p>Automations</p>
+                        </Paper>
+                    </a>
+                </Grid>
+                <Grid item lg={4} md={6} xs={12}>
+                    <a href="/guests">
+                        <Paper className={classes.paper}>
+                            <img alt="Guests" className="dashboard-nav-img"
+                                 src="/img/icons/dashboard/icon-guests-bw.svg"/>
+                            <p>Guests</p>
+                        </Paper>
+                    </a>
+                </Grid>
+                <Grid item>
+                    <a href="/sharedWithMe">
+                        <Paper className={classes.paper}>
+                            <img alt="Shared-With-Me" className="dashboard-nav-img"
+                                 src="/img/icons/dashboard/icon-house-guest.svg"/>
+                            <p>Shared with me</p>
+                        </Paper>
+                    </a>
+                </Grid>
+
             </Grid>
-            <Grid item>
-                <a href="/house">
-                    <Paper className={classes.paper}>
-                        <img alt="Rooms"className="dashboard-nav-img" src="/img/icons/dashboard/icon-all-rooms-bw.svg"/>
-                        <p>Rooms</p>
-                    </Paper>
-                </a>
-            </Grid>
-            <Grid item>
-                <a href="/scenes">
-                    <Paper className={classes.paper}>
-                        <img alt="Scenes" className="dashboard-nav-img" src="/img/icons/dashboard/icon-scenes-bw.svg"/>
-                        <p>Scenes</p>
-                    </Paper>
-                </a>
-            </Grid>
-            <Grid item>
-                <a href="/automation">
-                    <Paper className={classes.paper}>
-                        <img alt="automation" className="dashboard-nav-img" src="/img/icons/dashboard/icon-automation-bw.svg"/>
-                        <p>Automation</p>
-                    </Paper>
-                </a>
-            </Grid>
-            <Grid item>
-                <a href="/guests">
-                    <Paper className={classes.paper}>
-                        <img alt="Guests" className="dashboard-nav-img" src="/img/icons/dashboard/icon-guests-bw.svg"/>
-                        <p>Guests</p>
-                    </Paper>
-                </a>
-            </Grid>
-            <Grid item>
-                <a href="/sharedWithMe">
-                    <Paper className={classes.paper}>
-                        <img alt="Shared-With-Me" className="dashboard-nav-img" src="/img/icons/dashboard/icon-house-guest.svg"/>
-                        <p>Shared With Me</p>
-                    </Paper>
-                </a>
-            </Grid>
-        </Grid>
+        </div>
     );
 }
 
 export {Dashboard as default}
+
