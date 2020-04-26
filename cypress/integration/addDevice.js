51e7d496fa922efc9a6b5ba184772f404eb8af22
Stefano Taillefert <taills@usi.ch>
2020-04-26 13:08:27 +0200

    56: Add fixtures and server routes

--- cypress/integration/addDevice.js
+++ cypress/integration/addDevice.js
@@ -1,3 +1,4 @@
+/// <reference types="cypress" />
 
 describe('test the add device page', function () {
     this.beforeEach(() => {
@@ -243,33 +244,49 @@ describe('test the add device page', function () {
             .should('include', '/devices')
     })
 
-it('testing the request', function () {  
-        
+    it('testing the request', function () {
+        cy.server()
+        cy.route({
+            url: 'http://localhost:8080/devices',
+            method: "GET",
+            response: "fixture:devices.json"
+        })
+        cy.route({
+            url: 'http://localhost:8080/rooms',
+            method: "GET",
+            response: "fixture:getRooms.json"            
+        })
+        cy.route({
+            url: 'http://localhost:8080/auth/validate',
+            method: "POST",
+            headers: {
+                username: "user1",
+                "session-token": "sdjvayusd6asdyasgdi7a"
+            },
+            response: "user1"
+        })
+        cy.route({
+            url: 'http://localhost:8080/devices',
+            method: "POST",
+            headers: {
+                username: "user1",
+                "session-token": "sdjvayusd6asdyasgdi7a"
+            },
+            body: {name: "asd", icon: "/img/icons/devices/bulb-led.svg", type: 2, roomId: "2"},
+            status: 201,
+            response: {}
+        })        
+
         cy.visit('/addDevice')
         cy.get('input[type=text]')
-                .type('newLight')
+            .type('newLight')
         cy.get('select').first()
             .select('Light')
         cy.get('select').eq(1)
             .select('room1')
         cy.get('.btn-primary')
             .click()
-
-        cy.server()
-        cy.request({
-            url: ' http://localhost:8080/devices',
-            body: { name: "light",
-                    icon: "/img/icons/devices/bulb-regular.svg",
-                    type: 1,
-                    roomId: "1"
-                }
-        })
-        .then(function(response){
-            expect(response.status).to.eq(200)
-        })
-
-
-
-
-        })  
+        cy.url()
+            .should('include', '/devices')
+    })
 })  
