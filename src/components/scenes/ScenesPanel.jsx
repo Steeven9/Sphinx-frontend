0e661997751ccc93b2f9bbbaef629754e7ab3bad
Aron Dalle Pezze <dallea@usi.ch>
2020-05-11 20:48:09 +0200

    #111 removed comment in ScenesPanel.jsx

--- src/components/scenes/ScenesPanel.jsx
+++ src/components/scenes/ScenesPanel.jsx
@@ -14,7 +14,6 @@ const host = window.location.protocol + '//' + window.location.hostname + ':8080
 const scenesFetchUrl = host + '/scenes';
 const guestScenesFetchUrl = host + '/guests/' + params.get('owner') + '/scenes';
 let fetchUrl;
-// const fetchOwnerUrl = host + '/guests/houses' + params.get('owner'); //Uncomment this and delete next line when the API route is ready
 const fetchOwnerUrl = window.location.protocol + '//' + window.location.hostname + ':8080/guests/houses';
 
 /**
