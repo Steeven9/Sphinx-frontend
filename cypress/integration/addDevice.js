8d30d12cc9f9382c4a979b58dce6b8283fc2eb3b
Stefano Taillefert <taills@usi.ch>
2020-05-23 11:50:42 +0200

    #115: Stop misusing stuff (ty @lucaponzanelli)

--- cypress/integration/addDevice.js
+++ cypress/integration/addDevice.js
@@ -2,65 +2,67 @@
 
 describe('test the add device page', function () {
     this.beforeEach(() => {
-        cy.Login()
-    })
+        cy.fixture('headers.json').then((headers) => {
+            cy.server();
+            cy.route({
+                url: 'http://localhost:8080/devices',
+                method: "GET",
+                headers: headers,
+                response: "fixture:devices.json"
+            });
+            cy.route({
+                url: 'http://localhost:8080/rooms',
+                method: "GET",
+                headers: headers,
+                response: "fixture:rooms.json"
+            }).as('getRooms');
+
+            cy.route({
+                url: 'http://localhost:8080/auth/validate',
+                method: "POST",
+                headers: headers,
+                response: "user1"
+            });
+            cy.route({
+                url: 'http://localhost:8080/devices',
+                method: "POST",
+                headers: headers,
+                body: { name: "asd", icon: "/img/icons/devices/bulb-led.svg", type: 2, roomId: "2" },
+                status: 201,
+                response: {}
+            });
+        });
+
+        cy.Login();
+    });
 
     it('testing all the buttons', function () {
-        cy.visit('/addDevice')
-        cy.contains('Add device')
+        cy.visit('/addDevice');
+        cy.contains('Add device');
 
         cy.get('.btn-secondary').first()
-            .click()
+            .click();
         cy.url()
-            .should('include', '/devices')
-        cy.visit('/addDevice')
+            .should('include', '/devices');
+        cy.visit('/addDevice');
 
         cy.get('.btn-primary')
-            .click()
-        cy.contains('Please insert all information')
-    })
+            .click();
+        cy.contains('Please insert all information');
+    });
 
-    it('testing the request', function () {
-        let headers = cy.getHeaders();
-
-        cy.server()
-        cy.route({
-            url: 'http://localhost:8080/devices',
-            method: "GET",
-            headers: headers,
-            response: "fixture:devices.json"
-        })
-        cy.route({
-            url: 'http://localhost:8080/rooms',
-            method: "GET",
-            headers: headers,
-            response: "fixture:rooms.json"            
-        })
-        cy.route({
-            url: 'http://localhost:8080/auth/validate',
-            method: "POST",
-            headers: headers,
-            response: "user1"
-        })
-        cy.route({
-            url: 'http://localhost:8080/devices',
-            method: "POST",
-            headers: headers,
-            body: {name: "asd", icon: "/img/icons/devices/bulb-led.svg", type: 2, roomId: "2"},
-            status: 201,
-            response: {}
-        })        
-
-        cy.visit('/addDevice')
+    it.only('testing the request', function () {
+        cy.visit('/addDevice');
+        cy.wait('@getRooms');
         cy.get('input[type=text]')
-            .type('newLight')
+            .type('newLight');
         cy.get('select').first()
-            .select('Light')
+            .select('Light');
         cy.get('select').eq(1)
-            .select('room1')
+            .select('room1');
         cy.get('.btn-primary')
-            .click()
+            .click();
         cy.url()
-            .should('include', '/devices')
-    })
+            .should('include', '/devices');
+    });
 })  
